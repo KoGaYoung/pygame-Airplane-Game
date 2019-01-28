5757966bf79dc1f0478657a36cf828403fdd75bd
KoGaYoung <36693355+KoGaYoung@users.noreply.github.com>
2019-01-28 16:25:58 +0900

    test

--- PyGame/mainScreen.py
+++ PyGame/mainScreen.py
@@ -27,7 +27,10 @@ def main():
         elapsed = clock.tick(30)
         #score += elapsed
         #print(ballList[1].x, ballList[1].y)
-
+        print("공[1]좌표")
+        print(ballList[1].x, ballList[1].y)
+        print("둘 사이의 x거리구하기")
+        print(player.x-ballList[1].x)
         #키가 눌려있을 때 동시키 입력할 수 있도록
         for event in pygame.event.get():
             if(event.type == KEYDOWN):
